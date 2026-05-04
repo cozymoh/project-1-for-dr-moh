fe9889e3ff90cfe55171085ad6304004c79c7ae1
cozymoh <mohamed.gk.ohhh@gmail.com>
2026-05-04 17:07:28 +0300

    Update main.cpp
    switch case isnt being a good person, if statement used instead with for loops

--- main.cpp
+++ main.cpp
@@ -1,16 +1,49 @@
 #include <iostream>
 using namespace std;
-
-const int max = 100; //more than enough 
-const int cap = 50;
-
+const int max=20; //more than enough 
 struct Song {
-    char title[cap];
-    char artist[cap];
+string title;
+string artist;
 };
-
 Song playlist[max] = {
     {"Beat It", "Michael Jackson"},
     {"Billie Jean", "Michael Jackson"},
-    {"Smooth Criminal", "Michael Jackson"}
-}; // continue after tea and biscuits 
+    {"Smooth Criminal", "Michael Jackson"}};
+ // continue after tea and biscuits 
+int countSongs=3;
+void addSong(){//add a new one
+if(countSongs>=max){
+cout<<"Playlist full\n";
+return;
+}
+
+string t,ar;
+
+cout<<"Enter Title:";
+cin>>t;
+
+cout<<"Enter Artist:";
+cin>>ar;
+
+for(int i=0;i<countSongs;i++){//checking if song is already there
+if(playlist[i].title==t){
+cout<<"Duplicate Song!\n";
+return;
+}
+}
+playlist[countSongs].title=t;
+playlist[countSongs].artist=ar;
+countSongs++;
+cout<<"Song Added\n";
+}
+void searchByID(){// search for it by id 
+int id;
+cout<<"Enter ID:";
+cin>>id;
+if(id<1||id>countSongs){
+cout<<"Invalid ID\n";
+return;
+}
+cout<<"Title:"<<playlist[id-1].title<<"\n";
+cout<<"Artist:"<<playlist[id-1].artist<<"\n";
+}
