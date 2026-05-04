1dbeb2690e06ad4366e384eb266f90375d29bde0
cozymoh <mohamed.gk.ohhh@gmail.com>
2026-05-04 15:19:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+const int max = 100; //more than enough 
+const int cap = 50;
+
+struct Song {
+    char title[cap];
+    char artist[cap];
+};
+
+Song playlist[max] = {
+    {"Beat It", "Michael Jackson"},
+    {"Billie Jean", "Michael Jackson"},
+    {"Smooth Criminal", "Michael Jackson"}
+}; // continue after tea and biscuits 
